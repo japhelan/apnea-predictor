b5299a24acf185249a566e629131dbf6f8433719
Jack Phelan <89405607+japhelan@users.noreply.github.com>
2025-12-14 10:30:34 -0800

    Added utils module - rearranged data_prep notebook to use utils functions

---
+++ utils/data_utils.py
@@ -0,0 +1,41 @@
+'''
+Docstring for python.functions
+This module contains various utility functions used throughout the project.
+'''
+
+import pandas as pd
+import numpy as np
+
+def add_multi_index(df, display_names):
+    """
+    Adds a multi-level index to the DataFrame columns using display names.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame.
+    display_names (dict): A dictionary mapping column names to display names.
+
+    Returns:
+    pd.DataFrame: DataFrame with multi-level column index.
+    """
+    new_columns = []
+    for col in df.columns:
+        if col in display_names:
+            new_columns.append((display_names[col], col))
+        else:
+            new_columns.append(('', col))
+    
+    df.columns = pd.MultiIndex.from_tuples(new_columns)
+    return df
+
+def inspect_structure(df):
+    """
+    Prints the structure of the DataFrame including column names and data types.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame.
+    """
+    print(f"Structure of {df.name}:")
+    print("Shape: ", df.shape)
+    print(df.info())
+    print("\nColumn Names:")
+    print(df.columns.tolist())
