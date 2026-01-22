b1a660fe30534ab54362dd97ce4f8f4a7559f04b
Jack Phelan <89405607+japhelan@users.noreply.github.com>
2026-01-22 14:12:57 -0800

    Reverting dbt edits 1.1

--- dbt_internal_packages/dbt-databricks/macros/relations/table/rename.sql
+++
@@ -1,4 +0,0 @@
--- funcsign: (relation, string) -> string
-{% macro databricks__get_rename_table_sql(relation, new_name) %}
-  ALTER TABLE {{ relation.render() }} RENAME TO `{{ new_name }}`
-{% endmacro %}
